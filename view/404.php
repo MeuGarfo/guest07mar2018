b86ac9aeefa0ac298faf120d845ebf63a37f6582
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-03-07 09:16:00 -0300

    D  .gitignore D  .htaccess D  controller/Home.php D  controller/Message.php D  example.env D  model/messages D  router.php D  view/404.php D  view/home/read.php D  view/layout.php D  view/message/create.php D  view/message/list.php

--- view/404.php
+++
@@ -1,7 +0,0 @@
-<?php
-$data['title']='Erro 404';
-$data['content']=<<<heredoc
-<h1>{$data['title']}</h1>
-Página não encontrada.
-heredoc;
-$View->view(VIEW_DIR.'layout', $data);
--- view/home/read.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data['title']='Guest';
-$messageCreateForm=$View->view(VIEW_DIR.'message/create', [], false);
-$messages=$View->view(VIEW_DIR.'message/list', $data, false);
-$data['content']=<<<heredoc
-<h2>Mensagens</h2>
-heredoc;
-$data['content']=$data['content'].$messages.$messageCreateForm;
-$View->view(VIEW_DIR.'layout', $data);
